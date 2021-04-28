fe611c2d7698008f60661a38307c0b3ba0e439af
fordpic <fordpickert@gmail.com>
2021-04-28 13:00:53 -0500

    app scaled out, bio compenent complete

---
+++ src/Components/Bio.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Bio = () => {
+	return (
+		<div className='bio'>
+			<h3>Hey there! My name is Ford Pickert & welcome to my site!</h3>
+			<p>
+				I am a full stack software engineer who enjoys applying msyelf to solve
+				unique & challenging issues, particularly if it is for the betterment of
+				the planet. My primary focuses currently are blockchain technology &
+				music tech startups, as both are about to be revolutionized. Take a look
+				around the site, & please don't be stranger if you have any comments /
+				questions!
+			</p>
+		</div>
+	);
+};
+
+export default Bio;
