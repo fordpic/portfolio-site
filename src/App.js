bde38c319415247d56c77d30d05ed1f86aefaac4
fordpic <fordpickert@gmail.com>
2021-05-05 12:55:41 -0500

    more animation work

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Bio from './Components/Bio';
 import Contact from './Components/Contact';
 import Projects from './Components/Projects';
-import ProjCard from './Components/ProjCard';
 
 function App() {
 	return (
