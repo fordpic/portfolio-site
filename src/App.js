f1aa393d40ee4d67a52289abda2e696dd09fd745
fordpic <fordpickert@gmail.com>
2021-05-11 16:53:07 -0500

    clean up

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Bio from './Components/Bio';
 import Contact from './Components/Contact';
 import Projects from './Components/Projects';
+import Currently from './Components/Currently';
+import { Route, Link } from 'react-router-dom';
 
 function App() {
 	return (
