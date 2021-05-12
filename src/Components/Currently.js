80cdfa7ef62b93b8a58dc91c5af5d1851a97266b
fordpic <fordpickert@gmail.com>
2021-05-11 20:20:55 -0500

    list styling

--- src/Components/Currently.js
+++ src/Components/Currently.js
@@ -4,33 +4,36 @@ const Currently = () => {
 	return (
 		<div className='currently'>
 			<h3>Currently, I'm working on:</h3>
-			<ul>
+			<ul className='current-items'>
 				<li>
 					<img
 						src='https://image.flaticon.com/icons/png/512/824/824918.png'
 						alt='Blockchain'></img>
+					<br />
 					Blockchain
 				</li>
 				<li>
 					<img
 						src='https://solidity.readthedocs.io/en/develop/_images/logo.svg'
 						alt='Solidity'></img>
+					<br />
 					Solidity
 				</li>
 				<li>
 					<img
-						src='https://www.pinclipart.com/picdir/middle/537-5374089_react-js-logo-clipart.png'
+						src='https://cdn.worldvectorlogo.com/logos/react-1.svg'
 						alt='React'></img>
+					<br />
 					React
 				</li>
 				<li>
-					<img
-						src='https://image.pngaaa.com/282/619282-middle.png'
-						alt='Python'></img>
+					<img src='https://i.imgur.com/jNKe3I5.png' alt='Python'></img>
+					<br />
 					Python
 				</li>
 				<li>
 					<img src='https://i.imgur.com/GX0qzK1.jpg' alt='Web3'></img>
+					<br />
 					Web3.js
 				</li>
 			</ul>
